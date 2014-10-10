f9ac94681b3a05ca73d2e5cc8f84f32009038dab
Kevin Most <kevin@kevinmost.com>
2014-10-09 20:45:55 -0400

    Commented FireEmblemEntities class

--- core/src/com/basecolon/FireJemblem/ashley/entity/FireEmblemEntities.java
+++ core/src/com/basecolon/FireJemblem/ashley/entity/FireEmblemEntities.java
@@ -26,11 +26,18 @@ public enum FireEmblemEntities {
     ;
 
     FireEmblemEntities(List<Class<? extends Component>> components) {
+        // We define our components in a List in our enum constructor, but then it is immediately turned into an array.
+        // We do this because we can't create a generic array in Java, so we create a generic List and then turn it into an array
+        // Of course, we need to add the following line to suppress compiler warnings
         //noinspection unchecked
         this.components = components.toArray(new Class[components.size()]);
     }
     private Class<? extends Component>[] components;
 
+    /**
+     * Get all of the components that make up this enum element. Returns as an array because the {@link Family#getFor(Class[])} method takes varargs, not a List
+     * @return An array containing all of the components that make up this enum
+     */
     public Class<? extends Component>[] getComponents() {
         return components;
     }
